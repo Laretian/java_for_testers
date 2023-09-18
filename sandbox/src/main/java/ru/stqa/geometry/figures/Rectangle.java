ef2918b0fdb416d8b2fdcd18804c46b1f562df59
Artem Tokar <laretia@rambler.ru>
2023-09-18 19:20:50 +0300

    Создание объектов, замена class на record

--- sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
@@ -1,6 +1,24 @@
 package ru.stqa.geometry.figures;
 
-public class Rectangle {
+public record Rectangle(double a, double b) {
+
+//    public static void printRectangleArea(double a, double b) {
+//        var text = String.format("Площадь прямоугольника со сторонами %f и %f = %f", a, b, rectangleArea(a, b));
+//        System.out.println(text);
+//    }
+//
+//    private static double rectangleArea(double a, double b) {
+//        return a * b;
+//    }
+
+//    private double a;
+//    private double b;
+//
+//    public Rectangle(double a, double b) {
+//        this.a = a;
+//        this.b = b;
+//    }
+
     public static void printRectangleArea(double a, double b) {
         var text = String.format("Площадь прямоугольника со сторонами %f и %f = %f", a, b, rectangleArea(a, b));
         System.out.println(text);
--- sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
@@ -1,12 +1,36 @@
 package ru.stqa.geometry.figures;
 
-public class Square {
-    public static void printSquareArea(double side) {
-        String text = String.format("Площадь квадрата со стороной %f = %f", side, squareArea(side));
+public record Square(double side) {
+
+//    public static void printSquareArea(double side) {
+//        String text = String.format("Площадь квадрата со стороной %f = %f", side, area(side));
+//        System.out.println(text);
+//    }
+//
+//    public static double area(double a) {
+//        return a * a;
+//    }
+//
+//    public static double perimeter(double a) {
+//        return 4 * a;
+//    }
+
+    public static void printSquareArea(Square s) {
+        String text = String.format("Площадь квадрата со стороной %f = %f", s.side, s.area());
         System.out.println(text);
     }
 
-    private static double squareArea(double a) {
-        return a * a;
+//    private double side;
+//
+//    public Square(double side) {
+//        this.side = side;
+//    }
+
+    public double area() {
+        return this.side * this.side;
+    }
+
+    public double perimeter() {
+        return 4 * this.side;
     }
 }
