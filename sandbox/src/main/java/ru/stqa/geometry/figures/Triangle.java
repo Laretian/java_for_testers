e8986c463ad7be4a5979de93d5ec0d0e87a97504
Artem Tokar <laretia@rambler.ru>
2023-10-04 14:45:09 +0300

    Добавлен тест сравнения сторон треугольников

--- sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -1,18 +1,12 @@
 package ru.stqa.geometry.figures;
 
+import java.util.Objects;
+
 import static java.lang.Math.sqrt;
 
-public class Triangle {
+public record Triangle(double a, double b, double c) {
 
-    private double a;
-    private double b;
-    private double c;
-
-    public Triangle(double a, double b, double c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-
+    public Triangle {
         if (a < 0 || b < 0 || c < 0) {
             throw new IllegalArgumentException("Triangle side should be is non-negative");
         } else if ((a + b < c) || (a + c < b) || (b + c < a)) {
@@ -29,4 +23,18 @@ public class Triangle {
         return (a + b + c);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Triangle triangle = (Triangle) o;
+        return (Double.compare(this.a, triangle.a) == 0 && Double.compare(this.b, triangle.b) == 0 && Double.compare(this.c, triangle.c) == 0)
+                || (Double.compare(this.b, triangle.a) == 0 && Double.compare(this.c, triangle.b) == 0 && Double.compare(this.a, triangle.c) == 0)
+                || (Double.compare(this.c, triangle.a) == 0 && Double.compare(this.a, triangle.b) == 0 && Double.compare(this.b, triangle.c) == 0);
+    }
+
+    @Override
+    public int hashCode() {
+        return 1;
+    }
 }
--- sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
@@ -18,11 +18,33 @@ public class TriangleTests {
     @Test
     void cannotCreateTriangleWithNegativeSide() {
         try {
-            new Triangle(-5, 7, 9);
+            new Triangle(-5.0, 7.0, 9.0);
             Assertions.fail();
         } catch (IllegalArgumentException exception) {
             // OK
         }
+    }
+
+    @Test
+    void testEquality() {
+        var t1 = new Triangle(3.0, 4.0, 5.0);
+        var t2 = new Triangle(3.0, 4.0, 5.0);
+        var t3 = new Triangle(3.0, 4.0, 5.0);
+
+        Assertions.assertEquals(t1, t2);
+        Assertions.assertEquals(t1, t3);
+        Assertions.assertEquals(t2, t3);
+    }
+
+    @Test
+    void testEquality2() {
+        var t1 = new Triangle(3.0, 4.0, 5.0);
+        var t2 = new Triangle(4.0, 5.0, 3.0);
+        var t3 = new Triangle(5.0, 3.0, 4.0);
+
+        Assertions.assertEquals(t1, t2);
+        Assertions.assertEquals(t1, t3);
+        Assertions.assertEquals(t2, t3);
     }
 
 }
